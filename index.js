bfbf3898d18a5ccf6e4addd6d3d54c5860d3745b
Rodd-droid <rodd_brian@outlook.com>
2024-10-10 08:27:08 -0500

    feat: :art: Actualizacion de animaciones 09/10

--- index.js
+++ index.js
@@ -16,3 +16,39 @@ document.addEventListener("DOMContentLoaded", function () {
     }, options);
     observer.observe(hero);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const oferta = document.querySelector('.oferta');
+    const options = {
+      threshold: 0.7
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                $('.oferta-logo').addClass('presentation');
+            }
+        });
+    }, options);
+    observer.observe(oferta);
+});
+
+$('.link-inicio').on('click', function(event) {
+    event.preventDefault();
+    document.getElementById('inicio').scrollIntoView({ behavior: 'smooth' }); 
+});
+
+$('.link-nosotros').on('click', function(event) {
+    event.preventDefault();
+    document.getElementById('nosotros').scrollIntoView({ behavior: 'smooth' }); 
+});
+
+$('.link-productos').on('click', function(event) {
+    event.preventDefault();
+    document.getElementById('productos').scrollIntoView({ behavior: 'smooth' }); 
+});
+
+$('.link-contacto').on('click', function(event) {
+    event.preventDefault();
+    document.getElementById('contacto').scrollIntoView({ behavior: 'smooth' }); 
+});
